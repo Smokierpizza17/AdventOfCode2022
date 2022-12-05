7e83d1344f9cf85636a65c6758364e30c65fe4a0
Smokierpizza17 <oskar.s.munro@gmail.com>
2022-12-05 16:38:02 +0100

    day 5 part 1 complete; solving part 2

---
+++ 5_SupplyStacks/part1.py
@@ -0,0 +1,42 @@
+import re
+
+with open("/home/oskar/Documents/AoC2022/day5/input.txt", "r") as inputFile:
+    taskInput = inputFile.read().split("\n")
+
+crateStacks = {}
+
+class CrateStack():
+    def __init__(self, name):
+        crateStacks[name] = self
+        self.crates = list()
+    def moveCrateTo(self, targetStack):
+        targetStack.takeCrate(self.crates[-1])
+        self.crates.pop(-1)
+    def takeCrate(self, crateStr):
+        self.crates.append(crateStr)
+
+lastLineOfStacks = taskInput.index("") - 2  # blank line divides stacks from numbers and instructions
+numbersString = taskInput[lastLineOfStacks + 1] 
+
+numbers = [int(i) for i in re.findall(r"\d+", numbersString)]
+for i in range(1, max(numbers)+1):
+    CrateStack(str(i))
+
+stacksRaw = taskInput[:lastLineOfStacks+1]
+stacksRaw.reverse()
+
+for line in stacksRaw:
+    for i in range(max(numbers)):
+        if 4*i+1 < len(line) and line[4*i+1] != " ":
+            stackName = str(i + 1)
+            crateStacks[stackName].takeCrate(line[4*i+1])
+
+for line in taskInput[lastLineOfStacks + 3:]:
+    count, fromStack, toStack = re.findall(r"\d+", line)
+    for _ in range(int(count)):
+        crateStacks[str(fromStack)].moveCrateTo(crateStacks[str(toStack)])
+
+topCrates = []
+for stack in crateStacks.values():
+    topCrates.append(stack.crates[-1])
+print("".join(topCrates))
